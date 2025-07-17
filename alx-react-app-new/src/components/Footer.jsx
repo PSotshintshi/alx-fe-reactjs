29aa5db0ceb57a2b4de2a6a2377ff5c9308b94cc
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-07-17 20:52:08 +0200

    new commits

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <div>
-            <footer>
+            <footer style={{ backgroundColor: 'blue', color: 'pink', textAlign: 'center' }}>
                  <p>© 2023 City Lovers</p>
             </footer>
         </div>
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent (){
     return(
-        <div>
+        <div style={{ border: '1px green', padding: '10px', margin: '10px' }}>
             <main>
                 <p>I love to visit New York, Paris, and Tokyo.</p>
             </main>
