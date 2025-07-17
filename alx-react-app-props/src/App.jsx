ddd1e11c030b86f5b3d617a8ab160c52c697a45e
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-07-17 22:08:56 +0200

    new props files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,7 +4,10 @@ import UserContext from './components/UserContext'
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return 
+   <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
 }
 
 export default App;
