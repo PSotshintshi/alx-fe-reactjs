1788680b3eb705cc15ac97965a698df95c374a16
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-08-11 21:17:35 +0200

    tailwind css commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,11 +2,11 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-10 sm:my-12 md:my-20 rounded-lg shadow-lg text-center">
       <img
-        className="w-24 h-24 md:w-36 md:h-36 mx-auto rounded-full sm:w-24 sm:h-24"
+        className="w-24 h-24 md:w-36 md:h-36 mx-auto rounded-full sm:w-24 sm:h-24 hover:scale-110 transition-transform duration-300 ease-in-out"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-sm md:text-base text-gray-600">
