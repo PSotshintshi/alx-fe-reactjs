c092836afaf48571123999caa5c591547b993c62
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-08-13 20:01:21 +0200

    recipe sharing platform setup

---
+++ recipe-sharing-platform/src/components/tailwind.config.js
@@ -0,0 +1,12 @@
+
+function tailwindConfig(){
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}};
+export default tailwindConfig;
