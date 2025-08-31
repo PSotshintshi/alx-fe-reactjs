9b1589366fe144ca7fad2376b709da6e69b461fe
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-08-31 19:53:45 +0200

    Updating the components

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
-import Post from "./components/Post";
+import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
           }
         />
 
-        <Route path="/post/:id" element={<Post />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
